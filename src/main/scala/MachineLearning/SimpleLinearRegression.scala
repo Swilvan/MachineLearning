dcfd0f51f4b976169d6f4472030c641468b3b91a
Silvan van Leeuwen <silvansmeel@gmail.com>
2016-06-03 13:53:50 +0200

    Replace tuples by usage of Point

--- src/main/scala/MachineLearning/SimpleLinearRegression.scala
+++ src/main/scala/MachineLearning/SimpleLinearRegression.scala
@@ -1,6 +1,6 @@
 package MachineLearning
 
-class SimpleLinearRegression(testData: Set[(Double, Double)], alpha: Double = 0.05) {
+class SimpleLinearRegression(testData: Set[Point], alpha: Double = 0.05) {
 
   val precision = 0.000001
 
@@ -9,8 +9,8 @@ class SimpleLinearRegression(testData: Set[(Double, Double)], alpha: Double = 0.
   val bestFit: Line = gradientDescent(Line(origin = 0, slope = 0))
 
   def gradientDescent(hypothesis: Line): Line = {
-    val newOrigin = hypothesis.origin - alpha * factor * testData.map { case (x, y) => hypothesis.origin + hypothesis.slope * x - y }.sum
-    val newSlope = hypothesis.slope - alpha * factor * testData.map { case (x, y) => (hypothesis.origin + hypothesis.slope * x - y) * x }.sum
+    val newOrigin = hypothesis.origin - alpha * factor * testData.map ( point => hypothesis.origin + hypothesis.slope * point.x - point.y ).sum
+    val newSlope = hypothesis.slope - alpha * factor * testData.map ( point => (hypothesis.origin + hypothesis.slope * point.x - point.y) * point.x ).sum
     val newHypothesis: Line = Line(newOrigin, newSlope)
     if ((newOrigin - hypothesis.origin).abs <= precision && (newSlope - hypothesis.slope).abs <= precision) newHypothesis
     else gradientDescent(newHypothesis)
@@ -21,7 +21,7 @@ object Main extends App {
 
 
   override def main(args: Array[String]) {
-    val x = new SimpleLinearRegression(Set((1d, 1d), (2d, 2d))).bestFit
+    val x = new SimpleLinearRegression(Set(Point(1,1), Point(2,2))).bestFit
     println(x)
   }
 }
